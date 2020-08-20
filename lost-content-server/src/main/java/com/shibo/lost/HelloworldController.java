52181692b245e15b07eea347406a6b2b6c54149a
shibo <shibo@daojia-inc.com>
2020-08-20 17:50:35 +0800

    add lost-content-server

---
+++ lost-content-server/src/main/java/com/shibo/lost/HelloworldController.java
@@ -0,0 +1,11 @@
+package com.shibo.lost;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author by shibo on 2020/8/20.
+ */
+@Controller
+public class HelloworldController {
+    
+}
---
+++ lost-content-server/src/main/java/com/shibo/lost/LostContentServerApplication.java
@@ -0,0 +1,13 @@
+package com.shibo.lost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostContentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostContentServerApplication.class, args);
+    }
+
+}
