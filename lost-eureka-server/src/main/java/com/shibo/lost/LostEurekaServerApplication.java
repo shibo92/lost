c2bc81ded0ffc8d424c9847ec8fb6f89c224a749
shibo <shibo@daojia-inc.com>
2020-08-20 15:20:49 +0800

    euerka-server

---
+++ lost-eureka-server/src/main/java/com/shibo/lost/LostEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.shibo.lost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostEurekaServerApplication.class, args);
+    }
+
+}
---
+++ lost-eureka-server/src/test/java/com/shibo/lost/LostEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shibo.lost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
