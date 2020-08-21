633ac0c9b1c314092650b67e8a3961d64ebe5269
shibo <shibo@daojia-inc.com>
2020-08-21 14:11:02 +0800

    config

--- lost-config-server/src/main/java/com/shibo/lost/LostConfigServerApplication.java
+++ lost-config-server/src/main/java/com/shibo/lost/LostConfigServerApplication.java
@@ -3,10 +3,12 @@ package com.shibo.lost;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class LostConfigServerApplication {
 
     public static void main(String[] args) {
