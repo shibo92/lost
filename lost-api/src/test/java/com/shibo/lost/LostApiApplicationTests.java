1f69f24512dab8aeadb2621da968f128644e4956
shibo <shibo@daojia-inc.com>
2021-11-27 15:23:02 +0800

    ``

--- lost-api/src/test/java/com/shibo/lost/LostApiApplicationTests.java
+++ lost-api/src/test/java/com/shibo/lost/LostApiApplicationTests.java
@@ -1,5 +1,9 @@
 package com.shibo.lost;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.context.ContextUtil;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +12,18 @@ class LostApiApplicationTests {
 
     @Test
     void contextLoads() {
+        ContextUtil.enter("entranceOne", "appA");
+        Entry nodeA = null;
+        try {
+            nodeA = SphU.entry("nodeA");
+        } catch (BlockException e) {
+            e.printStackTrace();
+        } finally {
+            if (nodeA != null) {
+                nodeA.exit();
+            }
+        }
+        ContextUtil.exit();
     }
 
 }
